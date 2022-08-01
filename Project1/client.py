4ff2876fc47f0bfc8ffbf5c54d4d1c125e186cbb
diegoalvarez17 <diegoalvarez1237@hotmail.com>
2022-07-31 22:51:13 -0600

    It worked 2 times :c

--- Project1/client.py
+++ Project1/client.py
@@ -4,36 +4,117 @@
 # Documentation: 
 # https://slixmpp.readthedocs.io/en/latest/
 # https://oriolrius.cat/wp-content/uploads/2009/10/Oreilly.XMPP.The.Definitive.Guide.May.2009.pdf
-
 import asyncio
 import logging
+from getpass import getpass
+from argparse import ArgumentParser
 
-from slixmpp import ClientXMPP
+import slixmpp
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-class EchoBot(ClientXMPP):
+class SendMsgBot(slixmpp.ClientXMPP):
 
-    def __init__(self, jid, password):
-        ClientXMPP.__init__(self, jid, password)
+    """
+    A basic Slixmpp bot that will log in, send a message,
+    and then log out.
+    """
 
-        self.add_event_handler("session_start", self.session_start)
-        self.add_event_handler("message", self.message)
+    def __init__(self, jid, password, recipient, message):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
 
-    def session_start(self, event):
+        # The message we wish to send, and the JID that
+        # will receive it.
+        self.recipient = recipient
+        self.msg = message
+
+        # The session_start event will be triggered when
+        # the bot establishes its connection with the server
+        # and the XML streams are ready for use. We want to
+        # listen for this event so that we we can initialize
+        # our roster.
+        self.add_event_handler("session_start", self.start)
+
+    async def start(self, event):
+        """
+        Process the session_start event.
+
+        Typical actions for the session_start event are
+        requesting the roster and broadcasting an initial
+        presence stanza.
+
+        Arguments:
+            event -- An empty dictionary. The session_start
+                     event does not provide any additional
+                     data.
+        """
         self.send_presence()
-        self.get_roster()
+        await self.get_roster()
 
-    def message(self, msg):
-        if msg['type'] in ('chat', 'normal'):
-            msg.reply("Thanks for sending\n%(body)s" % msg).send()
+        self.send_message(mto=self.recipient,
+                          mbody=self.msg,
+                          mtype='chat')
+
+        self.disconnect()
 
 
 if __name__ == '__main__':
+    # Setup the command line arguments.
+    parser = ArgumentParser(description=SendMsgBot.__doc__)
 
-    logging.basicConfig(level=logging.DEBUG,
+    # Output verbosity options.
+    parser.add_argument("-q", "--quiet", help="set logging to ERROR",
+                        action="store_const", dest="loglevel",
+                        const=logging.ERROR, default=logging.INFO)
+    parser.add_argument("-d", "--debug", help="set logging to DEBUG",
+                        action="store_const", dest="loglevel",
+                        const=logging.DEBUG, default=logging.INFO)
+
+    # JID and password options.
+    parser.add_argument("-j", "--jid", dest="jid",
+                        help="JID to use")
+    parser.add_argument("-p", "--password", dest="password",
+                        help="password to use")
+    parser.add_argument("-t", "--to", dest="to",
+                        help="JID to send the message to")
+    parser.add_argument("-m", "--message", dest="message",
+                        help="message to send")
+
+    args = parser.parse_args()
+
+    # Setup logging.
+    logging.basicConfig(level=args.loglevel,
                         format='%(levelname)-8s %(message)s')
 
-    xmpp = EchoBot('somejid@example.com', 'use_getpass')
+    if args.jid is None:
+        args.jid = input("Username: ")
+    if args.password is None:
+        args.password = getpass("Password: ")
+    if args.to is None:
+        args.to = input("Send To: ")
+    if args.message is None:
+        args.message = input("Message: ")
+
+    # Setup the EchoBot and register plugins. Note that while plugins may
+    # have interdependencies, the order in which you register them does
+    # not matter.
+
+    xmpp = SendMsgBot(args.jid, args.password, args.to, args.message)
+    xmpp.register_plugin('xep_0030') # Service Discovery
+    xmpp.register_plugin('xep_0199') # XMPP Ping
+
+    # Connect to the XMPP server and start processing XMPP stanzas.
     xmpp.connect()
-    xmpp.process()
+    xmpp.process(forever=False)
+
+
+
+    # email = input("Email: ")
+    # password = input("Password: ")
+    # messageTo = input("EmailToSend: ")
+    # messageToBeSend = input("Type a message: ")
+
+    # # Setup the EchoBot and register plugins. Note that while plugins may
+    # # have interdependencies, the order in which you register them does
+    # # not matter.
+    # xmpp = SendMsgBot(email, password, messageTo, messageToBeSend)
