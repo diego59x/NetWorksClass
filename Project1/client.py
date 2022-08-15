566eeb0a41bfda269d54f20a387a186e47e1ee56
diegoalvarez17 <diegoalvarez1237@hotmail.com>
2022-08-14 22:05:07 -0600

    trying to integrate threads

--- Project1/client.py
+++ Project1/client.py
@@ -6,6 +6,8 @@
 # https://oriolrius.cat/wp-content/uploads/2009/10/Oreilly.XMPP.The.Definitive.Guide.May.2009.pdf
 import asyncio
 import logging
+from pickle import FALSE
+from threading import Thread
 import slixmpp
 import xmpp
 from slixmpp.xmlstream import ET
@@ -13,6 +15,19 @@ from slixmpp.exceptions import IqError, IqTimeout
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
+class XMPPNotifications(slixmpp.ClientXMPP):
+
+
+    def __init__(self, jid, password):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+
+        self.user = jid
+        self.add_event_handler("message", self.messageNotifications)
+
+    async def messageNotifications(self, event):
+        message = event['body']
+        message_from = event['from']
+        print("%s says: %s" % (message_from, message) )
 class XMPPChat(slixmpp.ClientXMPP):
 
 
@@ -22,6 +37,9 @@ class XMPPChat(slixmpp.ClientXMPP):
         self.user = jid
         self.add_event_handler("session_start", self.start)
 
+    def addUser(self, email):
+        self.send_presence_subscription(email)
+        print("User Added")
 
     def deleteUser(self):
         resp = self.Iq()
@@ -67,15 +85,21 @@ class XMPPChat(slixmpp.ClientXMPP):
 
     def seeConnections(self, name, jid):
         connections = self.client_roster.presence(jid)
+        connected = False
+        # This works only if an user is connected
         for res, pres in connections.items():
             # 'res' is the id of their device
             show = 'available'
             # Default value available
+            connected = True
         
             print('Name: %s (%s)' % (name, show))
             # Users with a different status
             if pres['status']:
                 print('%s' % pres['status'])
+        # Show if the contact is away
+        if (connected == False):
+            print("Name: %s (offline)" % jid)
 
     def notificationMessage(self, emisor, state):
         message = self.Message()
@@ -85,14 +109,14 @@ class XMPPChat(slixmpp.ClientXMPP):
         message.send()
 
     async def start(self, event):
-        self.send_presence()
+        self.send_presence('chat','Hi')
 
         print("Welcome! ", self.user)
         await self.get_roster()
-        # 2 4 5 6 8 9
+        # 5 
         menu = True
         while menu == True:
-            print("1. See Users Info \n2. Add Friend \n3. See 1 User Info \n4. Send Private Message \n5. Send Group Message \n6. Set Presence Message\n7. Log Out\n8. Delete Account")#9. Send notifications\n 10. Send files")
+            print("1. See Users Info \n2. Add Friend \n3. See 1 User Info \n4. Send Private Message \n5. Send Group Message \n6. Set Presence Message\n7. Log Out\n8. Delete Account")#9. Send notifications
             option = input("")
 
             if option == "1":
@@ -101,6 +125,7 @@ class XMPPChat(slixmpp.ClientXMPP):
             elif (option == "2"):
                 print("--- Add a friend ---")
                 user = input("Type email to add ")
+                self.addUser(user)
 
             elif (option == "3"):
                 print("--- See info of a friend ---")
@@ -113,13 +138,25 @@ class XMPPChat(slixmpp.ClientXMPP):
                 message = input("Message: ")
 
                 self.send_message(mto=user, mbody=message, mtype='chat')
+                await asyncio.sleep(0.5)
 
                 self.notificationMessage(user, 'paused')
 
             elif (option == "5"):
+                group = input("Type group to send message: ")
                 message = input("Message: ")
+
+                # group = group + "@conference.alumchat.fun"
+
+                self.send_message(mto=group, mbody=message, mtype='groupchat')
+                await asyncio.sleep(0.5)
+
+                # self.notificationMessage(group, 'paused')
+
             elif (option == "6"):
                 newPresenceMsg = input("Type new presence message: ")
+                self.send_presence('chat', newPresenceMsg)
+
             elif (option == "7"):
                 print("Have a nice day! :D")
                 menu = False
@@ -130,7 +167,7 @@ class XMPPChat(slixmpp.ClientXMPP):
                 self.deleteUser()
             else: 
                 print("Try another option!")
-        
+
 
 if __name__ == '__main__':
 
@@ -149,13 +186,18 @@ if __name__ == '__main__':
             # email = "alvarez@alumchat.fun"
             # password = "swais"
 
+            # email = "jojo@alumchat.fun"
+            # password = "jojo"
+            # t1 = threading.Thread(target=threadNotifications, name="t1", args=(email, password))
+            # t1.start()
+
             logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
             login = XMPPChat(email, password)
             login.register_plugin('xep_0030') # Service Discovery
             login.register_plugin('xep_0199') # XMPP Ping
             login.register_plugin("xep_0085")
             login.register_plugin("xep_0133")
-
+            login.register_plugin('xep_0045') # Mulit-User Chat (MUC)
 
             # Connect to the XMPP server and start processing XMPP stanzas.
             login.connect(disable_starttls=True)
